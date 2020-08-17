06471e6998fcfdfde94b0304a3bba5b64fe2ac03
mona <k.abildgaard.photography@gmail.com>
2020-08-17 13:11:11 -0700

    maps

--- Landing Page/landingpage.js
+++ Landing Page/landingpage.js
@@ -1,24 +1,22 @@
 $.ajax({
-    url: "https://www.themealdb.com/api/json/v1/1/random.php",
-    method: "GET"
+  url: "https://www.themealdb.com/api/json/v1/1/random.php",
+  method: "GET",
 }).then(function (data) {
-    console.log(data)
-    var output = data.meals[0].strMealThumb;
-    console.log(output)
+  console.log(data);
+  var output = data.meals[0].strMealThumb;
+  console.log(output);
 
-    output = data.meals[0].strCategory;
-    console.log(output)
+  output = data.meals[0].strCategory;
+  console.log(output);
 
-    output = data.meals[0].strInstructions;
-    console.log(output)
+  output = data.meals[0].strInstructions;
+  console.log(output);
 
-    output = data.meals[0].strSource;
-    console.log(output)
+  output = data.meals[0].strSource;
+  console.log(output);
 
-    for (var i = 0; i < data.meals.length; i++) {
-        meal.innerHTML +=
-
-        `<div class="rfimg" style=""><img class="rfimage" src="${data.meals[i].strMealThumb}" alt="${data.meals[i].strMealThumb}"></div> 
+  for (var i = 0; i < data.meals.length; i++) {
+    meal.innerHTML += `<div class="rfimg" style=""><img class="rfimage" src="${data.meals[i].strMealThumb}" alt="${data.meals[i].strMealThumb}"></div> 
         <br>
         <div class="rftitle" style="">${data.meals[i].strMeal}</div>
         <br>
@@ -32,32 +30,30 @@ $.ajax({
         </div>
         </div> 
         <br>
-        <div class="rfsource" style="">Source: <br> <a href="${data.meals[i].strSource}" target="_blank">${data.meals[i].strSource}</a></div>`;
-    }
-
+        <div class="rfsource" style="">Source: <br> <a href="${data.meals[i].strSource}" target="_blank">${data.meals[i].strSource}</a>
+        </div>`;
+  }
 });
 
 $.ajax({
-    url: "https://api.openbrewerydb.org/breweries?per_page=1",
-    method: "GET"
+  url: "https://api.openbrewerydb.org/breweries?per_page=1",
+  method: "GET",
 }).then(function (data) {
-    console.log(data)
-    var output = data[0].name;
-    console.log(output)
+  console.log(data);
+  var output = data[0].name;
+  console.log(output);
 
-    output = data[0].brewery_type;
-    console.log(output)
+  output = data[0].brewery_type;
+  console.log(output);
 
-    output = data[0].state;
-    console.log(output)
+  output = data[0].state;
+  console.log(output);
 
-    output = data[0].website_url;
-    console.log(output)
+  output = data[0].website_url;
+  console.log(output);
 
-    for (var i = 0; i < data.length; i++) {
-        drinks.innerHTML +=
-
-        `<div class="bfname" style="">${data[i].name}</div> 
+  for (var i = 0; i < data.length; i++) {
+    drinks.innerHTML += `<div class="bfname" style="">${data[i].name}</div> 
         <br>
         <div class="bfstate" style="">state:  ${data[i].state}</div> 
         <br>
@@ -76,8 +72,13 @@ $.ajax({
         </div>
         </div> 
         <br>
-        <div class="bfwebsite" style="">Wesbite: <br> <a href="${data[i].website_url}" target="_blank">${data[i].website_url}</a>
+        <div class="bfwebsite" style="">Website: <br> <a href="${data[i].website_url}" target="_blank">${data[i].website_url}</a>
         </div>`;
-        
-    }
+  }
 });
+
+//src="https://www.google.com/maps/embed/v1/location?key=AIzaSyAGSqgDLirtkOO-bg8zZAzCv4_h4tdhAa4
+//&q=${data[i].longitude}, ${data[i].latitude}">
+
+//src="https://www.google.com/maps/embed/v1/location?key=AIzaSyAGSqgDLirtkOO-bg8zZAzCv4_h4tdhAa4
+//&q=bfname">
