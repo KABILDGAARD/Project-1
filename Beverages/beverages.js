7504e070353a2208887b76f7ff8569c2de32b866
mona <k.abildgaard.photography@gmail.com>
2020-08-18 12:27:24 -0700

    beverages js done

--- Beverages/beverages.js
+++ Beverages/beverages.js
@@ -9,7 +9,7 @@ $("#searchicon").click(function () {
     var userInput = $("#ingredientsearch").val();
   
     //Variables used to create the request url
-    var URL = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+    var URL = "https://api.openbrewerydb.org/breweries?by_postal=";
     var jsonFormat = "&callback=JSON_CALLBACK";
     var ajaxURL = URL + userInput + jsonFormat;
   
@@ -17,39 +17,50 @@ $("#searchicon").click(function () {
       url: ajaxURL,
       method: "GET",
     }).then(function (data) {
-      console.log(data);
-      var output = data.meals[0].strMealThumb;
-      console.log(output);
-  
-      output = data.meals[0].strCategory;
-      console.log(output);
-  
-      output = data.meals[0].strInstructions;
-      console.log(output);
-  
-      output = data.meals[0].strSource;
-      console.log(output);
-  
-      for (var i = 0; i < data.meals.length; i++) {
-        meal.innerHTML += `<a id="modalbtn" class="uk-button uk-button-default" href="#modal-overflow" uk-toggle>${data.meals[i].strMeal}</a>
+        console.log(data);
+        var output = data[0].name;
+        console.log(output);
+      
+        output = data[0].brewery_type;
+        console.log(output);
+      
+        output = data[0].state;
+        console.log(output);
+      
+        output = data[0].website_url;
+        console.log(output);
+
+        output = data[0].longitude;
+        console.log(output);
+
+        output = data[0].latitude;
+        console.log(output);
+      
+        for (var i = 0; i < data.length; i++) {
+          drinks.innerHTML += 
+        
+        `<a id="modalbtn" class="uk-button uk-button-default" href="#modal-overflow" uk-toggle>${data[0].name}</a>
   
       <div id="modal-overflow" uk-modal>
           <div class="uk-modal-dialog">
   
               <div id="rfhead" class="uk-modal-header">
-                  <h2 id="rftitle" class="uk-modal-title">${data.meals[i].strMeal}</h2>
+                  <h2 id="rftitle" class="uk-modal-title">${data[0].name}</h2>
               </div>
       
               <div class="uk-modal-body" uk-overflow-auto>
-      
-                  <p>
-                  <div class="rfimg" style=""><img class="rfimage" src="${data.meals[i].strMealThumb}" alt="${data.meals[i].strMealThumb}"></div> 
-                  </p>
+    
+                  <iframe id="bfmap"
+                  width="450"
+                  height="450"
+                  src="https://www.google.com/maps/embed/v1/search?key=AIzaSyAGSqgDLirtkOO-bg8zZAzCv4_h4tdhAa4
+                    &q=${data[0].name}">
+                  </iframe>
                   <br>
-                  <p>${data.meals[i].strInstructions}</p>
+                  <p>${data[0].brewery_type}</p>
                   <br>
                   <p>
-                  <div class="rfsource" style="">Source: <br> <a href="${data.meals[i].strSource}" target="_blank">${data.meals[i].strSource}</a>
+                  <div class="rfsource" style="">Source: <br> <a href="${data[0].website_url}" target="_blank">${data[0].website_url}</a>
                 </div>    
                   </p>
                   <br>
